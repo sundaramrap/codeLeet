091fd05adf73ac6ea304e3991307aed79bac512c
sundaramrap <pragya.learn.28@gmail.com>
2025-07-17 23:22:54 +0530

    'findingLoopInLinkedList'

---
+++ day24_17thJuly25/141.linked-list-cycle.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=141 lang=javascript
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  let slow = head;
+  let fast = head;
+  if (slow === null && slow.next === null) return false;
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+};
+// @lc code=end
