ca4445f39cbd044729e8bd7c0e406952605d7a5f
sundaramrap <pragya.learn.28@gmail.com>
2025-08-01 14:13:56 +0530

    'recursionchp2'

--- day27_Aug1_25/index.js
+++ day27_Aug1_25/index.js
@@ -8,22 +8,57 @@
 
 // console.log(fibbo(5));
 
-function binarySearch(arr, target, low = 0, high = arr.length - 1) {
-  if (low > high) {
-    return -1;
-  }
+// function binarySearch(arr, target, low = 0, high = arr.length - 1) {
+//   if (low > high) {
+//     return -1;
+//   }
 
-  let mid = Math.floor((low + high) / 2);
+//   let mid = Math.floor((low + high) / 2);
 
-  if (arr[mid] === target) {
-    return mid;
-  } else if (target <= arr[mid]) {
-    return binarySearch(arr, target, low, mid - 1);
-  } else {
-    return binarySearch(arr, target, mid + 1, high);
+//   if (arr[mid] === target) {
+//     return mid;
+//   } else if (target <= arr[mid]) {
+//     return binarySearch(arr, target, low, mid - 1);
+//   } else {
+//     return binarySearch(arr, target, mid + 1, high);
+//   }
+// }
+
+// let arr = [1, 2, 4, 6, 8, 47, 80];
+// let biOut = binarySearch(arr, 8);
+// console.log(biOut);
+
+// console.log(`Print Number`);
+// function printN(n) {
+//   if (n <= 0) {
+//     return;
+//   }
+
+//   printN(n - 1);
+//   console.log(n);
+// }
+
+// printN(5);
+
+// function fibbo(n) {
+//   if (n == 0) {
+//     return 1;
+//   }
+
+//   return n * fibbo(n - 1);
+// }
+
+// let ansFibo = fibbo(4);
+
+// console.log(`The fibbo the number is ${ansFibo}`);
+
+function sumNo(number) {
+  if (Math.floor(number / 10) === 0) {
+    return number;
   }
+  console.log(`the num`, number);
+  return Math.floor(number % 10) + sumNo(Math.floor(number / 10));
 }
 
-let arr = [1, 2, 4, 6, 8, 47, 80];
-let biOut = binarySearch(arr, 8);
-console.log(biOut);
+let sumAns = sumNo(1342);
+console.log(`LEt the ans :${sumAns}`);
--- day27_Aug1_25/tempCodeRunnerFile.js
+++ day27_Aug1_25/tempCodeRunnerFile.js
@@ -7,3 +7,47 @@ function fibbo(n) {
 // }
 
 // console.log(fibbo(5));
+
+// function binarySearch(arr, target, low = 0, high = arr.length - 1) {
+//   if (low > high) {
+//     return -1;
+//   }
+
+//   let mid = Math.floor((low + high) / 2);
+
+//   if (arr[mid] === target) {
+//     return mid;
+//   } else if (target <= arr[mid]) {
+//     return binarySearch(arr, target, low, mid - 1);
+//   } else {
+//     return binarySearch(arr, target, mid + 1, high);
+//   }
+// }
+
+// let arr = [1, 2, 4, 6, 8, 47, 80];
+// let biOut = binarySearch(arr, 8);
+// console.log(biOut);
+
+// console.log(`Print Number`);
+// function printN(n) {
+//   if (n <= 0) {
+//     return;
+//   }
+
+//   printN(n - 1);
+//   console.log(n);
+// }
+
+// printN(5);
+
+// function fibbo(n) {
+//   if (n == 0) {
+//     return 1;
+//   }
+
+//   return n * fibbo(n - 1);
+// }
+
+// let ansFibo = fibbo(4);
+
+// console.log(`The fibbo the number is ${ansFibo}`);
