e98f6da9d1ac5231e70de65bd101230fce21b66d
sundaramrap <pragya.learn.28@gmail.com>
2025-08-13 11:45:30 +0530

    'bitmanuplation'

---
+++ bitWise/day32Aug13/index.js
@@ -0,0 +1,15 @@
+console.log(`...connecion check`);
+
+function bitFlip(num) {
+  let ans = "";
+
+  while (num >= 1) {
+    ans = (num % 2) + ans;
+
+    num = Math.floor(num / 2);
+  }
+
+  return ans;
+}
+
+console.log(bitFlip(23));
